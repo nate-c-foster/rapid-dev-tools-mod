085c39cb08163fd12debcac7b6acc48c64e36a47
Nate Foster <nate.foster@flexwareinnovation.com>
2024-03-16 10:36:20 -0500

    first commit

---
+++ common/src/main/java/dev/openscada/rapiddevtoolsmod/common/RapidDevToolsModModule.java
@@ -0,0 +1,6 @@
+package dev.openscada.rapiddevtoolsmod.common;
+
+public class RapidDevToolsModModule {
+    public static final String MODULE_ID = "dev.openscada.rapiddevtoolsmod.RapidDevToolsMod";
+
+}
